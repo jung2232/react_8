91436c7340b58ad2b7be1417c84c8bade3689a42
Hwang Tae Young <hty3466@naver.com>
2023-01-18 18:46:45 +0900

    test/브랜치 통합

---
+++ src/assets/icon/socialLogin/index.js
@@ -0,0 +1,4 @@
+import { ReactComponent as GoogleIcon } from "./googleIcon.svg";
+import { ReactComponent as KakaoIcon } from "./kakaoIcon.svg";
+
+export { GoogleIcon, KakaoIcon };
--- src/index.js
+++ src/index.js
@@ -2,29 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import Detail from "./components/Detail";
-import SignUp from "./components/Signup";
-import Login from "./components/Login";
 import Main from "./components/Main";
 import Upload from "./components/Upload";
-import { Provider } from "react-redux";
-import store from "./redux/config/configStore";
+
+import SignIn from "./page/Sign/SignIn";
+import SignUp from "./page/Sign/SignUp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/Detail" element={<Detail />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/Main" element={<Main />} />
-        <Route path="/Upload" element={<Upload />} />
-      </Routes>
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/detail" element={<Detail />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/main" element={<Main />} />
+      <Route path="/upload" element={<Upload />} />
+    </Routes>
+  </BrowserRouter>,
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
