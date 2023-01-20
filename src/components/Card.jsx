155c61041577e782f124d6d02179a925993e3d40
jung2322 <vlfjsm1l2@gmail.com>
2023-01-20 17:39:55 +0900

    삭제보튼

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-
+import { useDispatch } from "react-redux";
+import { __deleteDetailList } from "./../redux/modules/boardSlice";
 const Card = ({ board }) => {
   const { img, desc, id, porice, title } = board;
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   const onDeleteHandler = (e) => {
     e.stopPropagation();
     if (window.confirm("물품을 지울까요?")) {
