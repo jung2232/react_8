f0930d7ddc95d22931960e503a7b94f18e2b72d2
Hwang Tae Young <hty3466@naver.com>
2023-01-20 17:12:26 +0900

    style/글쓰기

--- src/components/board/Upload.jsx
+++ src/components/board/Upload.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { __addBoard } from "../../redux/modules/boardSlice";
+
+import { StInput } from "../../lib/signStyle";
 const Upload = () => {
   const dispatch = useDispatch();
 
@@ -13,6 +14,8 @@ const Upload = () => {
     name: "상품이름입니다",
     price: 0,
   });
+
+  const [iamgSrc, setimageSrc] = useState("");
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
@@ -30,90 +33,87 @@ const Upload = () => {
   const onChangeImg = async (e) => {
     e.preventDefault();
     if (e.target.files) {
+      const reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload = () => {
+        setimageSrc(reader.result);
+      };
       const uploadFile = e.target.files[0];
       const copy = { ...form, image: uploadFile };
       setForm(copy);
     }
   };
 
-  // const onCickImageUpload = () => {
-  //   imageInput.current.click();
-  // };
-  // if (isLoading) {
-  //   return <div>로딩 중....</div>;
-  // }
-  // //에러
-  // if (error) {
-  //   return <div>{error.message}</div>;
-  // }
   return (
     <>
-      <SbWrap>
+      <StWrap>
         <StForm onSubmit={onSubmitHandler}>
           <StImages>
-            <label htmlFor="profile-upload" />
-            <input
+            <div>
+              <img src={iamgSrc} alt="" />
+            </div>
+            <label htmlFor="profile-upload">이미지 등록</label>
+            <StInput
+              style={{ display: "none" }}
               type="file"
               id="profile-upload"
               accept="image/*"
               onChange={onChangeImg}
             />
           </StImages>
-          <StUser>
-            <StFont>
-              <p style={{ magin: "20px", fontSize: "24px" }}>상품명</p>
-              <br />
-              <input
-                type="text"
-                placeholder="상품명을 입력해 주세요."
-                value={form.title}
-                onChange={(ev) => {
-                  const { value } = ev.target;
-                  setForm({ ...form, title: value });
-                }}
-                required
-              />
-              <p style={{ magin: "20px", fontSize: "24px" }}>상품설명</p>
-              <StTextarea
-                type="text"
-                placeholder="상품내용을 입력해 주세요."
-                rows={20}
-                cols={30}
-                value={form.description}
-                q
-                onChange={(ev) => {
-                  const { value } = ev.target;
-                  setForm({ ...form, description: value });
-                }}
-                required
-              />
-              <div>
-                <StNumber
-                  type="number"
-                  placeholder="금액을 입력해 주세요."
-                  value={form.price}
-                  onChange={(ev) => {
-                    const { value } = ev.target;
-                    setForm({ ...form, price: value });
-                  }}
-                  required
-                />
-                <span style={{ maginLeft: "20px", fontSize: "20px" }}>
-                  가격
-                </span>
-              </div>
-            </StFont>
-          </StUser>
+          <StInputBox>
+            <p>상품명</p>
+            <StInput
+              type="text"
+              placeholder="상품명을 입력해 주세요."
+              value={form.title}
+              onChange={(ev) => {
+                const { value } = ev.target;
+                setForm({ ...form, title: value });
+              }}
+              required
+            />
+          </StInputBox>
+          <StInputBox>
+            <p>상품설명</p>
+            <StTextarea
+              type="text"
+              placeholder="상품내용을 입력해 주세요."
+              rows={20}
+              cols={30}
+              value={form.description}
+              q
+              onChange={(ev) => {
+                const { value } = ev.target;
+                setForm({ ...form, description: value });
+              }}
+              required
+            />
+          </StInputBox>
+          <StInputBox>
+            <p>가격</p>
+            <StNumber
+              type="number"
+              placeholder="금액을 입력해 주세요."
+              value={form.price}
+              onChange={(ev) => {
+                const { value } = ev.target;
+                setForm({ ...form, price: value });
+              }}
+              required
+            />
+          </StInputBox>
+
           <StButton>등록하기</StButton>
         </StForm>
-      </SbWrap>
+      </StWrap>
     </>
   );
 };
 
 export default Upload;
 
-const StNumber = styled.input`
+const StNumber = styled(StInput)`
   ::-webkit-inner-spin-button {
     -webkit-appearance: none;
     margin: 0;
@@ -125,50 +125,53 @@ const StNumber = styled.input`
 `;
 const StTextarea = styled.textarea`
   resize: none;
+  width: 100%;
 `;
 
 const StButton = styled.button`
+  border: none;
+  background: none;
   width: 100px;
-  height: 40px;
-  background: orange;
-  text-align: center;
-  cursor: pointer;
-  position: absolute;
-  bottom: 50px;
-  text-align: center;
+  padding: 8px 0;
+  box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1),
+    0 8px 10px -6px rgb(0 0 0 / 0.1);
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 15px;
 `;
 
-const StFont = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-const StBody = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const StImages = styled.div``;
-const StUser = styled.div`
-  width: 500px;
-  height: 1000px;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  flex-direction: column;
+const StImages = styled.div`
+  width: 20%;
+  img {
+    width: 100%;
+  }
 `;
 const StForm = styled.form`
+  width: 80%;
+  margin: 0 auto;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
 `;
-const StFile = styled.input``;
-const SbWrap = styled.div``;
-const SbLogo = styled.h2`
-  font-size: 30px;
-  color: #000;
-  margin: 40px;
+
+const StWrap = styled.div`
+  width: 100%;
 `;
 
-///
+const StInputBox = styled.div`
+  display: flex;
+  width: 80%;
+  margin: auto;
+  margin-bottom: 10px;
+  p {
+    width: 120px;
+  }
+  &:last-child {
+  }
+  input {
+    width: 100%;
+    flex: 1 1 0%;
+  }
+  textarea {
+    flex: 1 1 0%;
+  }
+`;
