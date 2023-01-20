04d3e2db1eba67a8db5e057d463374122b226d7c
jung2232 <101534426+jung2232@users.noreply.github.com>
2023-01-20 17:59:48 +0900

    Update axiosInstance.jsx

--- src/config/axiosInstance.jsx
+++ src/config/axiosInstance.jsx
@@ -2,6 +2,9 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "http://carrot8.shop:8080/",
+  headers: {
+    Accept: "application/ json",
+  },
 });
 
 axiosInstance.interceptors.request.use(
